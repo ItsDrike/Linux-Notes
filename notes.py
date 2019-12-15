7524c9df8ad03c639ae789a3c952a57c11b2462f
koumakpet <koumakpet@protonmail.com>
2019-12-15 22:37:33 +0100

    Added OOP

--- notes.py
+++ notes.py
@@ -1,134 +1,182 @@
 #!/bin/python3
-from sys import argv
 import os
+from sys import argv
 from getpass import getuser
 
 NOTES_DIR = os.path.expanduser('~/Personal/Notes')
 
-params = argv[1].split(' ')
 
-show_help = params[0]
-listing = params[1]
-remove = params[2]
-category = params[3]
-file_name = params[4]
-extension = params[5]
-
-ignore = False
-
-if extension[0] == '.':
-    extension = extension[1:]
-elif extension == '':
-    extension = 'txt'
-
-def print_listing(path):
-    if os.path.exists(path):
-        os.system(f'ls {path}')
-    else:
-        print(f'No notes found under category: {category}')
-
-def ensure_dir(path):
-    if not os.path.isdir(path):
-        os.makedirs(path)
-
-def create_note(category, file_name, extension):
-    path = os.path.join(NOTES_DIR, category)
-    ensure_dir(path)
-    path = os.path.join(path, f'{file_name}.{extension}')
-    if not os.path.exists(path):
-        print('Creating new note..')
-        os.mknod(path)
-    return path
-
-def open_note(path):
-    print(f'Opening {file_name}.{extension} in category {category}')
-    os.system(f'gedit {path}')
-
-def remove_note(category, file_name, extension):
-    if category != '':
-        if file_name != '':
-            path = os.path.join(NOTES_DIR, category, f'{file_name}.{extension}')
-            if os.path.exists(path):
-                while True:
-                    remove = input(f'Remove note: {category}/{file_name}.{extension}? (Y/N): ').lower()
-                    if remove == 'y':
-                        os.remove(path)
-                        print(f'Removed note {category}/{file_name}.{extension}')
-                        break
-                    elif remove == 'n':
-                        print('User Aborted')
-                        break
+class Static(object):
+    #TODO: root check
+    def __init__(self, show_help, listing, remove, category, filename, extension):
+        self.Note = Note(category, filename, extension)
+        if not len(self.Note.raw_category.split('/..') + self.Note.raw_category.split('../')) > 2:
+            if show_help:
+                Static.show_help()
+            elif listing:
+                self.show_listing()
+            elif remove:
+                self.remove()
             else:
-                print('No such note')
+                self.open()
         else:
-            path = os.path.join(NOTES_DIR, category)
-            if os.path.isdir(path):
-                while True:
-                    remove = input(f'Remove CATEGORY: {category}? (Y/N): ').lower()
-                    if remove == 'y':
-                        os.system(f'rm -r {path}')
-                        print(f'Removed category {category}')
-                        break
-                    elif remove == 'n':
-                        print('User Aborted')
-                        break
+            print(f'The category cannot point above parent directory ({self.Note.raw_category} -> {self.Note.full_raw_category_path})')
+
+    @staticmethod
+    def show_help():
+        print('\nUSAGE:')
+        print(' -n [filename]         set the filename')
+        print(' -c [category]         specify the category the note is in')
+        print(' -e [extension]        specify the file extension of note ')
+        print(' -l                    for listing (can be  combined with -c for category specific')
+
+    def show_listing(self):
+        if os.path.exists(self.Note.full_raw_category_path):
+            os.system(f'ls --color=auto {self.Note.full_raw_category_path}')
+        else:
+            if not self.Note.raw_category == '':
+                print(f'No notes found under category: {self.Note.raw_category}')
             else:
-                print('No such caregory')
-    else:
-        if os.path.isdir(NOTES_DIR):
-            while True:
-                remove = input(f'Remove ALL NOTES? (Y/N): ').lower()
-                if remove == 'y':
-                    while True:
-                        remove = input(f'Are you sure (you are removing all saved notes)? (Y/N): ').lower()
-                        if remove == 'y':
-                            os.system(f'rm -r {NOTES_DIR}')
-                            print('Removed all saved notes')
-                            break
-                        elif remove == 'n':
-                            print('User Aborted')
-                            break
-                    break
-                elif remove == 'n':
-                    print('User Aborted')
-                    break
+                print('No notes found')
+
+    def remove(self):
+        if self.Note.raw_category == '' and self.Note.raw_filename == '':
+            # Remove all notes
+            if os.path.isdir(NOTES_DIR):
+                if Static.ask_for_confirmation('REMOVE ALL NOTES?'):
+                    if Static.ask_for_confirmation('Are you sure (will remove ALL saved notes)?'):
+                        Static.remove_all_notes()
+                        return True
+                print('User-Canceled')
+            else:
+                print('You have no saved notes (nothing to remove)')
+            return False
+        elif self.Note.raw_filename == '':
+            # Remove whole category
+            if os.path.isdir(self.Note.full_category_path):
+                category = self.Note.category
+                if Static.ask_for_confirmation(f'Will fully remove category {category} with all its notes'):
+                    self.Note.remove_category()
+                    return True
+                print('User-Canceled')
+            else:
+                print(f'No such category ({self.Note.category})')
+            return False
         else:
-            print('There are no saved notes to remove')
+            # Remove single note
+            if os.path.exists(self.Note.full_path):
+                path = self.Note.relative_path
+                if Static.ask_for_confirmation(f'Will remove note {path}'):
+                    self.Note.remove()
+                    return True
+                print('User-Canceled')
+            else:
+                print(f'No such note ({self.Note.relative_path})')
+            return False
 
-def print_help():
-    print('\nUSAGE:')
-    print(' -n [filename]         set the filename')
-    print(' -c [category]         specify the category the note is in')
-    print(' -e [extension]        specify the file extension of note ')
-    print(' -l                    for listing (can be  combined with -c for category specific')
+    def open(self):
+        if not os.path.exists(self.Note.full_path):
+            self.Note.create()
+        self.Note.open()
 
-if getuser() == 'root':
-    if remove == 'false' or (remove == 'true' and category != ''):
-        print('Warning, the command was executed as root, this is not advised, please switch to non-root user.')
-        print('In order to remove effects of this command and all root\'s saved notes, use "sudo note -r"\n')
-    else:
-        print('Removing all root\'s saved notes')
+    @staticmethod
+    def ask_for_confirmation(text):
+        while True:
+            confirmation = input(f'{text} (Y/N): ').lower()
+            if confirmation == 'y':
+                return True
+            elif confirmation == 'n':
+                return False
+            else:
+                continue
+
+    @staticmethod
+    def remove_all_notes():
+        #TODO: Change remove method
         os.system(f'rm -r {NOTES_DIR}')
-        ignore = True
+        print('All notes has been removed')
 
-if not ignore:
-    if show_help == 'true':
-        print_help()
-    elif remove == 'true':
-        remove_note(category, file_name, extension)
-    else:
-        if category == '':
-            category = 'General'
+    @staticmethod
+    def ensure_dir():
+        if not os.path.isdir(NOTES_DIR):
+            print('Creating main notes directory')
+            os.makedirs(NOTES_DIR)
 
-        ensure_dir(NOTES_DIR)
-        path = os.path.join(NOTES_DIR, category)
-
-        if listing == 'true':
-            print_listing(path)
+class Note(Static):
+    def __init__(self, category, filename, extension):
+        self.raw_category = category
+        # Set default category
+        if self.raw_category == '':
+            self.category = 'General'
         else:
-            if file_name == '':
-                file_name = 'main'
-                extension = '__init__.txt'
-                print('Main')
-            path = create_note(category, file_name, extension)
-            open_note(path)
+            self.category = self.raw_category
+
+        self.raw_filename = filename
+        # Set default filename
+        if self.raw_filename == '':
+            self.filename = 'main'
+        else:
+            self.filename = self.raw_filename
+
+        # Remove first dot in extension or default it to txt
+        if extension != '':
+            if extension[0] == '.':
+                self.extension = extension[1:]
+            else:
+                self.extension = extension
+        else:
+            self.extension = 'txt'
+
+        # Set some paths
+        self.relative_path = os.path.join(self.category, f'{self.filename}.{self.extension}')
+        self.full_path = os.path.join(NOTES_DIR, self.relative_path)
+        self.full_category_path = os.path.join(NOTES_DIR, self.category)
+        self.full_raw_category_path = os.path.join(NOTES_DIR, self.raw_category)
+
+    def open(self):
+        print(f'Opening {self.relative_path}')
+        os.system(f'gedit {self.full_path}')
+
+    def create(self):
+        if not os.path.isdir(self.full_category_path):
+            self.create_category()
+        print(f'Creating {self.relative_path}')
+        os.mknod(self.full_path)
+
+    def create_category(self):
+        Static.ensure_dir()
+        print(f'Creating Category {self.category}')
+        os.mkdir(self.full_category_path)
+
+    def remove(self):
+        print(f'Removing {self.relative_path}')
+        os.remove(self.full_path)
+
+    def remove_category(self):
+        print(f'Removing category {self.category}')
+        #TODO: Change remove method
+        os.system(f'rm -r {self.full_category_path}')
+
+if __name__ == '__main__':
+    params = argv[1].split(' ')
+
+    show_help = params[0]
+    if show_help == 'true':
+        show_help = True
+    else:
+        show_help = False
+    show_listing = params[1]
+    if show_listing == 'true':
+        show_listing = True
+    else:
+        show_listing = False
+    remove = params[2]
+    if remove == 'true':
+        remove = True
+    else:
+        remove = False
+    category = params[3]
+    filename = params[4]
+    extension = params[5]
+
+    S = Static(show_help, show_listing, remove, category, filename, extension)
